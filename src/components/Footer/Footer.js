baf6d3ce8405cbc1740b823f84799558dd68150f
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-07 10:05:22 +0300

    Footer components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import './Footer.scss'
+import React from 'react';
+//import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer py-4 bg-purple'>
+        <div className='container'>
+            <div className='footer-content text-white text-center'>
+                <p className='copyright-text font-rubik fw-4 ls-1'>©2023 CopyRight SoftTech. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
   )
 }
 
