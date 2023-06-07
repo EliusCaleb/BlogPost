961fcb74bccf499f2229523ca136ff4ec374dc2d
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-07 11:51:47 +0300

    added comment to global state using redux

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { SidebarProvider } from './context/sidebarContext';
 import { BlogsProvider } from './context/blogsContext';
 import { UserProvider } from './context/usersContext';
+import { CommentProvider } from './context/commentContext';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SidebarProvider>
-      <UserProvider>
-      <App />
-      </UserProvider>
-      <BlogsProvider>
-       
-      </BlogsProvider>
-   
-    </SidebarProvider>
+      <SidebarProvider>
+            <UserProvider>
+                <BlogsProvider>
+                    <CommentProvider>
+                        <App />
+                    </CommentProvider>
+                </BlogsProvider>
+            </UserProvider>
+        </SidebarProvider>
   </React.StrictMode>
 );
 
