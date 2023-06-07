a02772750ad7e654d81bc7394f8caaaa5a2ec559
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-07 13:00:53 +0300

    setting auth for the user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-
-import './App.scss';
-import { BrowserRouter as Router, Routes,Route} from "react-router-dom"
-import {Home,SingleBlog} from './pages';
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
-import Sidebar from './components/Sidebar/Sidebar';
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, SingleBlog, Login } from "./pages";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-       <Router>
-          <NavBar/>
-           <Sidebar/>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/blogs/:id' element= {<SingleBlog />} />
-          </Routes>
-          <Footer/>
-       </Router>
+      <Router>
+        <NavBar />
+        <Sidebar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<SingleBlog />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
