c5cf911483b0039a03abddf2aeec51c93ddf6df9
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-07 09:04:05 +0300

    setting sidebar using redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { SidebarProvider } from './context/sidebarContext';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <SidebarProvider>
     <App />
+    </SidebarProvider>
   </React.StrictMode>
 );
 
