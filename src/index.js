78220e4cd0f213e11c3ea675f7a30598c62d927a
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-07 11:21:46 +0300

    creating blog list component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SidebarProvider } from './context/sidebarContext';
+import { BlogsProvider } from './context/blogsContext';
 import App from './App';
 
 
@@ -8,7 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SidebarProvider>
-    <App />
+      <BlogsProvider>
+       <App />
+      </BlogsProvider>
+   
     </SidebarProvider>
   </React.StrictMode>
 );
